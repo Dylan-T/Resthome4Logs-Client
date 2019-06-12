89d49fa12202d9a2f3168c7600cf7f951cc8bb5e
thomsodyla1 <thomsodyla1@ecs.vuw.ac.nz>
2019-06-12 15:04:40 +1200

    added code to convert logging event into a json string

--- src/main/java/nz/ac/vuw/swen301/assignment3/client/Resthome4LogsAppender.java
+++ src/main/java/nz/ac/vuw/swen301/assignment3/client/Resthome4LogsAppender.java
@@ -1,77 +1,62 @@
 package nz.ac.vuw.swen301.assignment3.client;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.sun.javafx.util.Logging;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
 
 import java.net.URI;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ *{
+ *     "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+ *     "message": "application started",
+ *     "timestamp": {},
+ *     "thread": "main",
+ *     "logger": "com.example.Foo",
+ *     "level": "DEBUG",
+ *     "errorDetails": "string"
+ *   }
  */
 public class Resthome4LogsAppender extends AppenderSkeleton {
 
-    private final long maxSize = 10;
-    private long discardedLogs = 0;
-    private List<String> currentLogs;
-    private Layout layout;
-
-
-
-    public Resthome4LogsAppender(){
-
-    }
-
-    /**
-     * Returns the logs currently stored
-     * @return list containing the current logs, must not be modifiable
-     */
-    public List<String> getCurrentLogs() {
-        return Collections.unmodifiableList(this.currentLogs);
-    }
-
-    /**
-     * Returns the 10 most recent logs stored
-     * @return
-     */
-    public String[] getTopLogs() {
-        //Check if <10 logs stored
-        int numLogs = 10;
-        if(getCurrentLogs().size() < 10){
-            numLogs = getCurrentLogs().size();
-        }
-
-        //Add 10 most recent to array
-        String[] topLogs = new String[10];
-        for(int i = 0; i < numLogs; i++){
-            topLogs[i] = currentLogs.get(currentLogs.size()-1-i);
-        }
-        return topLogs;
-    }
+    public Resthome4LogsAppender(){ }
 
     @Override
     protected void append(LoggingEvent loggingEvent) {
         //Note that arrays of log events (and not just single log events) are processed.
         try {
+
             URIBuilder builder = new URIBuilder();
-            builder.setScheme("http").setHost("localhost:8080").setPath("/resthome4logs/logs")
-                    .setParameter("LogEvent", "");
+            builder.setScheme("http").setHost("localhost:8080").setPath("/resthome4logs/logs");
+//                    .setParameter("LogEvent", gson.toJson(loggingEvent));
             URI uri = builder.build();
 
             // create and execute the request
             HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(uri);
+            HttpPost request = new HttpPost(uri);
+
+            //            StringEntity params = new StringEntity("details={\"id\":\"" + "INSERTIDHERE" + "\",\"message\":\""+ loggingEvent.getMessage() +"\",\"timestamp\":\"" + loggingEvent.getTimeStamp() + "\",\"thread\":\"" + loggingEvent.getThreadName() + "\",\"logger\":\"" + loggingEvent.getLoggerName() + "\",\"level\":\"" + loggingEvent.getLevel() + "\",\"errorDetails\":\"" + loggingEvent.getThrowableInformation() + "\",} ");
+            StringEntity params = new StringEntity(formatEvent(loggingEvent).toString());
+            params.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            request.setEntity(params);
+
             HttpResponse response = httpClient.execute(request);
 
-            // this string is the unparsed web page (=html source code)
+            //Not sure what the response is
             String content = EntityUtils.toString(response.getEntity());
             System.out.println(content);
         }catch (Exception e){
@@ -79,13 +64,48 @@ public class Resthome4LogsAppender extends AppenderSkeleton {
         }
     }
 
-    /**
-     * resets the appender's memory and fields
-     */
+    public JsonObject formatEvent(LoggingEvent loggingEvent){
+        JsonObject json = new JsonObject();
+        json.addProperty("id", 1);
+        json.addProperty("message", (String)loggingEvent.getMessage());
+        json.addProperty("timestamp", loggingEvent.getTimeStamp());
+        json.addProperty("thread", loggingEvent.getThreadName());
+        json.addProperty("logger", loggingEvent.getLoggerName());
+        json.addProperty("level", loggingEvent.getLevel().toString());
+        json.addProperty("errorDetails", loggingEvent.getNDC());
+        return json;
+
+
+    }
+
+    public List<String> getLogs(int limit, String level){
+        try {
+            URIBuilder builder = new URIBuilder();
+            builder.setScheme("http").setHost("localhost:8080").setPath("/resthome4logs/logs")
+                    .setParameter("limit", ""+limit)
+                    .addParameter("level", level);
+
+            URI uri = builder.build();
+
+            // create and execute the request
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(uri);
+            HttpResponse response = httpClient.execute(request);
+
+            String logs = EntityUtils.toString(response.getEntity());
+            Gson gson = new Gson();
+            return gson.fromJson(logs, new ArrayList<LoggingEvent>().getClass());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    @Override
     public void close() {
-        currentLogs.clear();
-        discardedLogs = 0;
-        this.closed = true;
+
     }
 
     /**
@@ -93,14 +113,7 @@ public class Resthome4LogsAppender extends AppenderSkeleton {
      * @return
      */
     public boolean requiresLayout() {
-        return true;
+        return false;
     }
 
-    @Override
-    public void setLayout(Layout layout){
-        if(layout == null){
-            throw new IllegalArgumentException();
-        }
-        this.layout = layout;
-    }
 }
--- src/test/java/test/nz/ac/vuw/swen301/assignment3/client/Resthome4LogsAppenderTests.java
+++ src/test/java/test/nz/ac/vuw/swen301/assignment3/client/Resthome4LogsAppenderTests.java
@@ -1,11 +1,15 @@
 package test.nz.ac.vuw.swen301.assignment3.client;
 
+import com.google.gson.Gson;
 import nz.ac.vuw.swen301.assignment3.client.Resthome4LogsAppender;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.AfterClass;
 import org.junit.Assume;
 import org.junit.BeforeClass;
@@ -20,17 +24,17 @@ public class Resthome4LogsAppenderTests {
     private static final String LOGS_PATH = TEST_PATH + "/logs";
 
     //HELPER METHODS
-    @BeforeClass
-    public static void startServer() throws Exception {
-        Runtime.getRuntime().exec("mvn jetty:run");
-        Thread.sleep(3000);
-    }
-
-    @AfterClass
-    public static void stopServer() throws Exception {
-        Runtime.getRuntime().exec("mvn jetty:stop");
-        Thread.sleep(3000);
-    }
+//    @BeforeClass
+//    public static void startServer() throws Exception {
+//        Runtime.getRuntime().exec("mvn jetty:run");
+//        Thread.sleep(3000);
+//    }
+//
+//    @AfterClass
+//    public static void stopServer() throws Exception {
+//        Runtime.getRuntime().exec("mvn jetty:stop");
+//        Thread.sleep(3000);
+//    }
 
     private HttpResponse get(URI uri) throws Exception {
         HttpClient httpClient = HttpClientBuilder.create().build();
@@ -62,5 +66,31 @@ public class Resthome4LogsAppenderTests {
     public void testGet() throws Exception{
         Assume.assumeTrue(isServerReady());
         Resthome4LogsAppender appender = new Resthome4LogsAppender();// TODO: Check assignment 2 for test idea's
+
+    }
+
+
+    @Test
+    public void test(){
+        /**
+         * [
+         *   {
+         *     "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+         *     "message": "application started",
+         *     "timestamp": {},
+         *     "thread": "main",
+         *     "logger": "com.example.Foo",
+         *     "level": "DEBUG",
+         *     "errorDetails": "string"
+         *   }
+         * ]
+         */
+        Logger logger = Logger.getLogger("test1");
+        LoggingEvent logEvent = new LoggingEvent("", logger, Level.INFO, "Test", null);
+        Resthome4LogsAppender appender = new Resthome4LogsAppender();
+        String json = appender.formatEvent(logEvent).toString();
+        System.out.println(json);
+        System.out.println(logEvent.toString());
+        System.out.println(logEvent.getMessage());
     }
 }
